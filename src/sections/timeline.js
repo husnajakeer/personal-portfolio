4970b396640c02237feb25543d5ddcc0696ef98d
husnajakeer <husnajakeer@gmail.com>
2024-12-20 20:22:38 -0500

    add timeline component

--- src/sections/timeline.js
+++ src/sections/timeline.js
@@ -1,8 +1,31 @@
 import '../styles/timeline.css';
+import Event from "../components/event.js";
 
 function Timeline() {
   return (
     <>
+        <div class="title">TIMELINE</div>
+        <Event></Event>
+        <svg class="connector" xmlns="http://www.w3.org/2000/svg" width="11" height="90" viewBox="0 0 11 190" fill="none">
+          <path d="M5.97791 184.997L4.98345 5.00006" stroke="#85715B" stroke-width="9" stroke-linecap="round"/>
+        </svg>
+        <Event></Event>
+        <svg class="connector" xmlns="http://www.w3.org/2000/svg" width="11" height="90" viewBox="0 0 11 190" fill="none">
+          <path d="M5.97791 184.997L4.98345 5.00006" stroke="#85715B" stroke-width="9" stroke-linecap="round"/>
+        </svg>
+        <Event></Event>
+        <svg class="connector" xmlns="http://www.w3.org/2000/svg" width="11" height="90" viewBox="0 0 11 190" fill="none">
+          <path d="M5.97791 184.997L4.98345 5.00006" stroke="#85715B" stroke-width="9" stroke-linecap="round"/>
+        </svg>
+        <Event></Event>
+        <svg class="connector" xmlns="http://www.w3.org/2000/svg" width="11" height="90" viewBox="0 0 11 190" fill="none">
+          <path d="M5.97791 184.997L4.98345 5.00006" stroke="#85715B" stroke-width="9" stroke-linecap="round"/>
+        </svg>
+        <Event></Event>
+        <svg class="connector" xmlns="http://www.w3.org/2000/svg" width="11" height="90" viewBox="0 0 11 190" fill="none">
+          <path d="M5.97791 184.997L4.98345 5.00006" stroke="#85715B" stroke-width="9" stroke-linecap="round"/>
+        </svg>
+        <Event></Event> 
     </>
   );
 }
