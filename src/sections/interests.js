2a638503bfc7fb6bb254f7ab3cf59a689dc027de
husnajakeer <husnajakeer@gmail.com>
2024-12-25 19:51:14 -0500

    add responsiveness

--- src/sections/interests.js
+++ src/sections/interests.js
@@ -3,7 +3,7 @@ import InterestIcons from "../images/interests.png";
 function Interests() {
   return (
     <>
-        <div className="interest-container">
+        <div class="interest-container">
             <div class="interest-left">
                 <div class="interest-title">Where do my passions lie?</div>
                 <div class="interest-description">
@@ -13,9 +13,7 @@ function Interests() {
             <div class="interest-right">
                 <img class="interest-pic" src={InterestIcons}></img>
             </div>
-
         </div>
-
     </>
   );
 }
